aff2ec5381c92d6a64da518845b486582535294e
Tobias Gysi <tobias.gysi@inf.ethz.ch>
2019-02-26 14:02:10 +0100

    updated submodule fetching

--- get_submodules.sh
+++ get_submodules.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 git submodule init
 git submodule update
-(cd barvinok; git submodule init polylib; git submodule update polylib)
-(cd isl; git submodule init imath; git submodule update imath)
+(cd barvinok; git submodule init polylib; git submodule update polylib; cd ..)
+(cd isl; git submodule init imath; git submodule update imath; cd ..)
